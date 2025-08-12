bf0c9e4d55a60a8661f7a1d4ecddc5162993f99b
Chuan Zhen <tanchuanzhen21@gmail.com>
2025-08-12 22:14:44 +0800

    Update and rename 01 Histogram of Tweets to 01 Histogram of Tweets.sql

---
+++ DataLemur/Easy/01 Histogram of Tweets.sql
@@ -0,0 +1,15 @@
+WITH tweet_counts AS (
+  SELECT 
+    user_id,
+    COUNT(*) AS tweet_bucket
+  FROM tweets
+  WHERE tweet_date between '2022-01-01' AND '2022-12-31'
+  GROUP BY user_id
+)
+
+SELECT
+  tweet_bucket,
+  COUNT(*) AS users_num
+FROM tweet_counts
+GROUP BY tweet_bucket
+ORDER BY tweet_bucket;
